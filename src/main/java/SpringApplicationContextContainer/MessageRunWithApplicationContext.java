7b8469547f60f9f810766bee728468c15bf7f8e3
Alexandr Alexeev <fart82@ukr.net>
2018-07-31 21:11:47 +0300

    Create singleton,Applicatio and BeanFactory

--- src/main/java/SpringApplicationContextContainer/MessageRunWithApplicationContext.java
+++ src/main/java/SpringApplicationContextContainer/MessageRunWithApplicationContext.java
@@ -3,7 +3,7 @@ package SpringApplicationContextContainer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-public class MessageRunWithBeanFactory {
+public class MessageRunWithApplicationContext {
     public static void main(String[] args) {
         ApplicationContext context = new FileSystemXmlApplicationContext(
                 "E:\\projects IJ\\SpringDemo\\SpringDemo\\src\\main\\resources\\message-bean.xml");
