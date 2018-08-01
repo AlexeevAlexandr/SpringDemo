b933a64002ddbd4eee204c4e0e89959db1ae86cf
Alexandr Alexeev <fart82@ukr.net>
2018-08-01 23:02:01 +0300

    add AutowiringBeans

---
+++ src/main/java/AutowiringBeans/Exam.java
@@ -0,0 +1,29 @@
+package AutowiringBeans;
+
+import AutowiringBeans.AnswerChecker;
+
+public class Exam {
+    private AnswerChecker answerChecker;
+    private String subject;
+
+    public AnswerChecker getAnswerChecker(){
+        return answerChecker;
+    }
+
+    public void setAnswerChecker(AnswerChecker answerChecker){
+        this.answerChecker = answerChecker;
+        System.out.println("We've just set answerChecker using setter");
+    }
+
+    public String getSubject(){
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void examCheck(){
+        answerChecker.checkAnswer();
+    }
+}
