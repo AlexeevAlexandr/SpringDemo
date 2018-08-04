eabbd42e3f810fa17ed77f91e112594bbd0fe761
Alexandr Alexeev <fart82@ukr.net>
2018-08-04 18:49:28 +0300

    add Event Handling

--- src/main/java/EventHandling/MyStartEventHandler.java
+++ src/main/java/EventHandling/MyStartEventHandler.java
@@ -1,16 +1,10 @@
 package EventHandling;
 
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextStartedEvent;
 
-public class MyStartEventHandler implements ApplicationListener {
+public class MyStartEventHandler implements ApplicationListener <ContextStartedEvent>{
     public void onApplicationEvent(ContextStartedEvent event){
         System.out.println("We've received ContextStartedEvent...");
-    }
-
-    @Override
-    public void onApplicationEvent(ApplicationEvent applicationEvent) {
-
     }
 }
--- src/main/java/EventHandling/MyStopEventHandler.java
+++ src/main/java/EventHandling/MyStopEventHandler.java
@@ -1,16 +1,10 @@
 package EventHandling;
 
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextStoppedEvent;
 
-public class MyStopEventHandler implements ApplicationListener {
+public class MyStopEventHandler implements ApplicationListener <ContextStoppedEvent>{
     public void onApplicationEvent(ContextStoppedEvent event) {
         System.out.println("We've received ContextStoppedEvent...");
-    }
-
-    @Override
-    public void onApplicationEvent(ApplicationEvent applicationEvent) {
-
     }
 }
